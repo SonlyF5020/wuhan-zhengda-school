6418702ae7f10ab14d4c1ca1c0fd76cef129a956
Honglai <Honglai.Zhan@Vodafone.com.au>
2015-10-23 00:43:37 +1100

    moving cartoon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
   window.addEventListener("scroll", runOnScroll);
 
   var cartoonImg = document.createElement('img');
-  cartoonImg.src = 'http://7aro2k.com1.z0.glb.clouddn.com/xiaohuangren_two.png';
+  cartoonImg.src = 'http://7aro2k.com1.z0.glb.clouddn.com/moving-xiaohuangren.jpg';
   cartoonImg.className = 'drop-move';
   document.getElementsByTagName('body')[0].appendChild(cartoonImg);
 });
